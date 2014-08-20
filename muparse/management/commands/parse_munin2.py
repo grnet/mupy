97a204ba0bfd3029ac30ffdd585ac3ea0e352a37
Stauros Kroustouris <staurosk@noc.grnet.gr>
2014-08-20 15:34:24 +0300

    fix script to get data from munin2

--- muparse/management/commands/parse_munin2.py
+++ muparse/management/commands/parse_munin2.py
@@ -18,6 +18,7 @@ from django.core.management.base import NoArgsCommand
 from bs4 import BeautifulSoup
 import urllib
 from django.conf import settings
+from muparse.models import *
 
 MNODES = settings.MUNIN_NODES
 
@@ -36,29 +37,42 @@ class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         for mnode in MNODES:
             mnode_dict = mnode[1]
-            mnode_id = mnode[0]
             baseUrl = mnode_dict['url']
-            cgiPath = mnode_dict['cgi_path']
             soup = self.parseUrlSoup(baseUrl, "index.html")
             homePage = soup.find_all('span', attrs={'class': 'domain'})
             for nodeGroup in homePage:
                 ng_url = "%s/%s" %(baseUrl, nodeGroup.a.get('href'))
-                self.stdout.write('NodeCategory: %s\n' % nodeGroup.a.text)
+                ng,created = NodeGroup.objects.get_or_create(name="%s@%s"%(nodeGroup.a.text, mnode_dict['name']), url=ng_url)
+                # self.stdout.write('NodeCategory: %s\n' % ng.name)
                 nodegroupSoup = self.parseUrlSoup(ng_url, "")
                 nodes = nodegroupSoup.find('div', attrs={'id': 'content'})\
                                      .find('ul', recursive=False).find_all('li', recursive=False)
                 for node in nodes:
                     node_name = node.find('span', attrs={'class':'domain'}, recursive=False)
                     n_url = "%s/%s/%s" %(baseUrl, nodeGroup.a.text, node_name.a.get('href'))
-                    self.stdout.write('-Node: %s %s\n' % (node_name.a.text, n_url))
-
+                    n,created = Node.objects.get_or_create(name=node.a.text, url=n_url, group=ng)
+                    self.stdout.write('-Node: %s\n' % (n.name))
                     services_categories = node.find_all('ul', recursive=False)
                     # get inside node
                     for service_category in services_categories:
                         category_group = service_category.find_all('li')
                         # a node has graph categories (class=host) that have services (class=service)
+                        last_cat = None
                         for category in category_group:
-                            self.stdout.write('--GraphCategory: %s\n' % category.a.text)
-                            services = category.findChildren('span', attrs={'class': 'service'})
-                            for service in services:
-                                self.stdout.write('---Graph: %s\n' % service.a.text)
+                            cat = category.find('span', attrs={'class': 'host'})
+                            if cat:
+                                gc, created = GraphCategory.objects.get_or_create(name=cat.text)
+                                self.stdout.write('--GraphCategory: %s\n' % gc.name)
+                                last_cat = gc
+                            else:
+                                slug = category.a.get('href').replace(node_name.text, '').replace('.html', '')[1:].replace('/index', '')
+                                g, created = Graph.objects.get_or_create(name=category.a.text, slug=slug, category=last_cat)
+                                self.stdout.write('---Graph: %s\n' % (g.name))
+                                services = category.findChildren('span', attrs={'class': 'service'})
+                                for service in services:
+                                    graph_base_url = baseUrl + '/munin-cgi/munin-cgi-graph/' + nodeGroup.a.text + '/' + service.a.get('href').replace('.html', '').replace('/index', '')
+                                    pageurl = baseUrl + '/' + nodeGroup.a.text + '/' + service.a.get('href')
+
+                                    nodegraph, created = NodeGraphs.objects.get_or_create(node=n, graph=g, baseurl=graph_base_url, pageurl=pageurl)
+                                    #  this is the link for the four final graphs (day, week, month, year)
+                                    self.stdout.write('---Service: %s, %s\n' % (service.a.text, pageurl))

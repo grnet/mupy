78eaaa4b85a2cfcdf67282f14b39f43222c4377b
Stauros Kroustouris <staurosk@noc.grnet.gr>
2014-09-05 17:26:10 +0300

    add signal to create a new account with a new user As soon as a user is created, a new account object is created automatically and an email is sent from the app to admin
    Two settings must be set in the local_settings file:
    DEFAULT_FROM_EMAIL and the ADMINS tuple

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.cache import cache
+from django.db.models.signals import post_save
+from django.core.mail import send_mail
+from django.conf import settings
 from muparse.models import Node
 
 
@@ -24,6 +27,22 @@ class UserProfile(models.Model):
         cache.delete('user_%s_tree_cat' % (self.user.pk))
         super(UserProfile, self).save(*args, **kwargs)
 
-
     def __unicode__(self):
         return "%s:%s" %(self.user.username, self.get_nodes())
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created and not kwargs.get('raw', False):
+        UserProfile.objects.create(user=instance)
+        to_emails = [admin[1] for admin in settings.ADMINS]
+        send_mail(
+            '[Mupy] new user',
+            'New user created: %s. Give him some nodes to watch!' % (
+                instance.username
+            ),
+            settings.DEFAULT_FROM_EMAIL,
+            to_emails,
+            fail_silently=False
+        )
+
+post_save.connect(create_user_profile, sender=User, dispatch_uid='create_UserProfile')

e6c37d4d466ae1c3007523f3f7b686dfe5a2a16c
Kroustouris Stauros <staurosk@noc.grnet.gr>
2014-09-09 10:43:14 +0300

    licence

--- mupy/urls.py
+++ mupy/urls.py
@@ -1,4 +1,3 @@
-# Copyright (C) 2010-2014 GRNET S.A.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

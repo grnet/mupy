8708c6c09634f68ead373e19e7b2fc52b498f477
Kroustouris Stauros <staurosk@noc.grnet.gr>
2014-10-06 12:03:52 +0300

    check if munin nodes setting is a tupple

--- muparse/management/commands/utils.py
+++ muparse/management/commands/utils.py
@@ -1,12 +1,16 @@
 from django.conf import settings
 from muparse.models import MuninNodes
+from django.core.exceptions import ImproperlyConfigured
 
 
 def get_v2_nodes():
     MNODES = []
     for node in settings.MUNIN_NODES:
-        if node[1].get('version', 'v2') == 'v2':
-            MNODES.append(node)
+        if isinstance(node, tuple):
+            if node[1].get('version', 'v2') == 'v2':
+                MNODES.append(node)
+            else:
+                raise ImproperlyConfigured('Munin node should be a tupple')
     for node in MuninNodes.objects.filter(munin_version='v2'):
         MNODES.append((node.name, node.as_dict()))
     return MNODES
@@ -15,8 +19,11 @@ def get_v2_nodes():
 def get_v1_nodes():
     MNODES = []
     for node in settings.MUNIN_NODES:
-        if node[1].get('version', 'v2') == 'v1':
-            MNODES.append(node)
+        if isinstance(node, tuple):
+            if node[1].get('version', 'v2') == 'v1':
+                MNODES.append(node)
+        else:
+            raise ImproperlyConfigured('Munin node should be a tupple')
     for node in MuninNodes.objects.filter(munin_version='v1'):
         MNODES.append((node.name, node.as_dict()))
     return MNODES
